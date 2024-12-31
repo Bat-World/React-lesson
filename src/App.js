7112ba68451bf7aba011eb04c1d0c8e8ac0a3fe1
1A-engineer <drewuncle863724lu@gmail.com>
2024-12-31 11:08:07 +0800

    useState tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Moods  from "./components/Moods";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      Import Each component to app  <Counter/> <Input /> <Moods/>
+    </div> 
   );
 }
 
